62e04ecc1c94f7e0ef7e9636b6355c0edeeebffc
d.ihsan maulana <121476322+ihsnmlna@users.noreply.github.com>
2024-05-11 22:59:29 +0700

    Create ContainString.java

---
+++ ContainString.java
@@ -0,0 +1,15 @@
+public class ContainString {
+    public static void main(String[] args) {
+        String name = "Welcome to Membaca";
+        System.out.println( "Hasilnya : " +name.contains("Membaca"));
+        System.out.println( "Hasilnya : " +name.contains("to"));
+        System.out.println( "Hasilnya : " +name.contains("Hello"));
+
+
+    }
+}
+
+//Output:
+//Hasilnya : true
+//Hasilnya : true
+//Hasilnya : false
